de187a31f5d8b02aafc410db8cd198106eea2d2e
Balázs Szabó <balazs.szabo@gmail.com>
2020-06-11 12:45:17 +0000

    mock api

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,24 @@
+var fs = require('fs');
+
 module.exports = {
-    // options...
     devServer: {
-        disableHostCheck: true
-    }
+        disableHostCheck: true,
+        before: function(app, server, compiler) {
+            app.get('/investments', function(req, res) {
+                const content = fs.readFileSync('mock/invesmtents_list.json', 'utf8');
+                const resp = JSON.parse(content);
+                res.json(resp);
+            });
+            app.post('/investments', function(req, res) {
+                const content = fs.readFileSync('mock/invesmtent_create.json', 'utf8');
+                const resp = JSON.parse(content);
+                res.json(resp);
+            });
+            app.delete('/investments', function(req, res) {
+                const content = fs.readFileSync('mock/invesmtent_delete.json', 'utf8');
+                const resp = JSON.parse(content);
+                res.json(resp);
+            });
+        }
+  }
 }
